1dd807410d3c3bbd4b8434263a08419be4aa9ab2
Pratik Joshi <pjoshi@maprtech.com>
2016-04-06 11:51:45 -0700

    Adjusted Reader Writer to resolve all compilers.
    Changed the design of the code a bit to add individual classes for Readers, Writers and a Driver code.

--- PracticeCodes/MyRWProblem.java
+++ PracticeCodes/MyRWProblem.java
@@ -1,29 +1,28 @@
 /**
- * Created by pjoshi on 4/5/16.
+ * Basic implementation of reader writer problem.
  */
-public class MyRWProblem implements Runnable {
-    public void run(){
-
-    }
-
+public class MyRWProblem {
+    static RWDriver rwDriver;
     public static void main(String[] args) {
-
+        rwDriver = new RWDriver();
+        Thread t1 = new Thread(new Readers(rwDriver));
+        t1.start();
     }
 }
 
-class ReaderWriter {
+class RWDriver {
     private int readers = 0;
     private int writers = 0;
     private int writeRequest = 0;
 
     public synchronized void lockRead() throws InterruptedException {
-       while (this.writers > 0 || this.writeRequest > 0){
-           Thread.currentThread().wait();
-       }
+        while (this.writers > 0 || this.writeRequest > 0) {
+            Thread.currentThread().wait();
+        }
         readers++;
     }
 
-    public synchronized void unlockRead(){
+    public synchronized void unlockRead() {
         readers--;
         notifyAll();
     }
@@ -31,7 +30,7 @@ class ReaderWriter {
     public synchronized void lockWrite() throws InterruptedException {
         writeRequest++;
 
-        while (readers > 0 || writers > 0){
+        while (readers > 0 || writers > 0) {
             Thread.currentThread().wait();
         }
         writeRequest--;
@@ -42,4 +41,51 @@ class ReaderWriter {
         writers--;
         notifyAll();
     }
+}
+
+class Readers implements Runnable {
+    private RWDriver rwDriver;
+
+    public Readers(RWDriver rwd){
+        this.rwDriver = rwd;
+    }
+
+
+    public void run() {
+        try {
+            rwDriver.lockRead();
+            /* Do some read operation */
+
+            /* Finish the read operation */
+            rwDriver.unlockRead();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
+
+class Writers implements Runnable {
+
+    RWDriver rwDriver;
+
+    public Writers(RWDriver rwd){
+        this.rwDriver = rwd;
+    }
+
+    public void run() {
+        try {
+            rwDriver.lockRead();
+            /* Do some read operation */
+
+            /* Finish the read operation */
+            rwDriver.unlockRead();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
